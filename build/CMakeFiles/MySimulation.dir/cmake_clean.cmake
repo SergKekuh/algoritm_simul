64a9b8aa59efac4b432c1ad3e82a0b69c3c26fb4
SergKekuh <serg.kekuh@gmail.com>
2024-06-25 10:49:48 +0000

    Start

---
+++ build/CMakeFiles/MySimulation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySimulation.dir/main.cpp.o"
+  "CMakeFiles/MySimulation.dir/main.cpp.o.d"
+  "MySimulation"
+  "MySimulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySimulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MySimulation.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MySimulation.
+# This may be replaced when dependencies are built.
